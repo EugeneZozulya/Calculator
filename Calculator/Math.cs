78c4fa4a58b73a1f2571910290ead76afed433a7
EugeneZozulya <y.zozulya200129@gmail.com>
2023-12-09 13:24:19 +0200

    Implement math interface

---
+++ Calculator/Math.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculator
+{
+    public class Math : IMath
+    {
+        public double Add(double a, double b) => a + b;
+
+        public double Div(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new ArgumentException("You can't divide by zero!");
+            }
+
+            return a / b;
+        }
+
+        public double Mul(double a, double b) => a * b;
+
+        public double Sub(double a, double b) => a - b;
+    }
+}
