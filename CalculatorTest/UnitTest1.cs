3bb369bd265527f4a4cb633bf882ff1ca9b39387
EugeneZozulya <y.zozulya200129@gmail.com>
2023-12-09 13:26:24 +0200

    Add unit tests

---
+++ CalculatorTest/UnitTest1.cs
@@ -0,0 +1,56 @@
+namespace CalculatorTest
+{
+    [TestClass]
+    public class MathTest
+    {
+        Calculator.Math math = new Calculator.Math();
+
+        [TestMethod]
+        public void Add_Test()
+        {
+            var a = 34;
+            var b = 44;
+
+            var expectedResult = 78;
+            var actualResult = math.Add(a, b);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Sub_Test()
+        {
+            var a = 34;
+            var b = 44;
+
+            var expectedResult = -10;
+            var actualResult = math.Sub(a, b);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Mul_Test()
+        {
+            var a = 10;
+            var b = 44;
+
+            var expectedResult = 440;
+            var actualResult = math.Mul(a, b);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Div_Test()
+        {
+            var a = 440;
+            var b = 10;
+
+            var expectedResult = 44;
+            var actualResult = math.Div(a, b);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
